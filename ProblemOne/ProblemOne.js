c64b3fdaae5d62ec4213fc9317defccb7e726d25
RonaldZ <zielaznickizm@g.cofc.edu>
2014-04-01 09:59:59 -0400

    Forgot the comments for Javascript

--- ProblemOne/ProblemOne.js
+++ ProblemOne/ProblemOne.js
@@ -1,3 +1,12 @@
+/*
+	Project Euler - Problem One.
+	Author: Ronald Zielaznicki
+	Problem: If we list all the natural numbers below 10 that are multiples 
+		of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		Find the sum of all the multiples of 3 or 5 below 1000
+*/
+
+//finds the sum of the multiples of 3 or 5 up to number x
 function sumOfMultiples(x){
 	var sumReturn = 0;
 	
